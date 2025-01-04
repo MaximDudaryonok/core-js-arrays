acdc3bce8714a4b25c78e9f9be4c26725afcfd57
Maxim <maxim.dudaryonok@yandex.ru>
2025-01-04 23:28:20 +0200

    feat: add getIntervalArray, sumArrays, findElement, findAllOccurrences tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,10 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 })
+    .fill(0)
+    .map((_, index) => start + index);
 }
 
 /**
@@ -37,8 +39,19 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length >= arr2.length) {
+    return arr1.reduce((acc, item, index) => {
+      if (index > arr2.length - 1) acc.push(item);
+      else acc.push(item + arr2[index]);
+      return acc;
+    }, []);
+  }
+  return arr2.reduce((acc, item, index) => {
+    if (index > arr1.length - 1) acc.push(item);
+    else acc.push(item + arr1[index]);
+    return acc;
+  }, []);
 }
 
 /**
@@ -53,8 +66,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -71,9 +84,10 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((elem) => elem === item).length;
 }
+
 
 /**
  * Removes falsy values from the specified array.
