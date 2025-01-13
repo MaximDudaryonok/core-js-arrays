a946693331754900c95f3d715868080824f7090f
Maxim <maxim.dudaryonok@yandex.ru>
2025-01-14 00:29:04 +0200

    feat: add swapHeadAndTail task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -623,8 +623,14 @@ function sortDigitNamesByNumericOrder(arr) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  return arr.length % 2 === 0
+    ? [...arr.slice(arr.length / 2), ...arr.slice(0, arr.length / 2)]
+    : [
+      ...arr.slice(Math.ceil(arr.length / 2)),
+      ...arr.slice(Math.floor(arr.length / 2), Math.ceil(arr.length / 2)),
+      ...arr.slice(0, arr.length / 2),
+    ];
 }
 
 module.exports = {
