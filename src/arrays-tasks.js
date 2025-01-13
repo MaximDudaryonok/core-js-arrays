0944b4800a4c6999b3cff61e114125df6aaf184f
Maxim <maxim.dudaryonok@yandex.ru>
2025-01-13 16:37:13 +0200

    feat: add shiftArray, sortDigitNamesByNumericOrder tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -569,8 +569,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? arr.slice(-n).concat(arr.slice(0, arr.length - n))
+    : arr.splice(-n).concat(arr.slice(0, -n));
 }
 
 /**
@@ -586,8 +588,20 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const stringnums = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => stringnums.indexOf(a) - stringnums.indexOf(b));
 }
 
 /**
